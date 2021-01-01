7f6c4f4671f746c519a0ea656ff4173f2cfdad8c
Puneet Singh <99singhpunee99@gmail.com>
2021-01-01 21:19:53 +0530

    feat: output using table
    tried a new way of generating table

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const body = document.body;
 const bill = document.querySelector("#inp-bill");
 const amount = document.querySelector("#inp-amt");
 const table = document.querySelector("#tbl-output");
+table.style.display = "none";
+
 const output = document.querySelector("#heading-output");
 console.log(output);
 let inpBill, inpAmount, balance;
@@ -12,7 +15,6 @@ let denominations = [2000, 500, 100, 20, 10, 5, 1];
 
 
 function keyupHandler(key){
-    
     if(key.keyCode === 13){
         if(this.id === "inp-bill"){
             inpBill = bill.value;
@@ -27,57 +29,40 @@ function keyupHandler(key){
             calculate(balance);
             if(message !== ""){
                 output.innerText = message;
-            }else{
-                generateTable(table, nd, countOfNotes);
-                generateTableHead(table, ["Note", "quantity"] );
+                table.innerHTML =  ``;
+            }else{     
+                table.innerHTML =  ``;
+                rows = `<thead> 
+                    <tr>
+                        <th> Note </th>
+                    </tr>
+                    <tr>
+                        <th> No of notes </th>
+                    </tr>
+                </thead>`
+                nd.map((n, index) => {
+                    rows += `<tbody> 
+                    <tr>
+                        <td> ${n} </td>
+                    </tr>
+                    <tr>
+                        <td> ${countOfNotes[index]} </td>
+                    </tr>
+                    </tbody>`
+                })
+                table.innerHTML = rows;
+                table.style.display = "block";
+                
             }
             
         }
-    }
-
-    // console.log(inpBill);
-    // console.log(inpAmount);
-    // console.log(balance);
-   
+    } 
     
 }
 
 
 bill.addEventListener("keyup", keyupHandler);
 amount.addEventListener("keyup", keyupHandler);
-
-function generateTableHead(tbl, data){
-    let thead = tbl.createTHead();
-    let row = thead.insertRow();
-
-    for(let key of data){
-        let th = document.createElement("th");
-        let text = document.createTextNode(key);
-        th.appendChild(text);
-        row.appendChild(th);
-    }
-}
-
-function generateTable(tbl, note, count){
-    console.log("inside table generator");
-    console.log("Notes: " + note);
-    console.log("count: " + count);
-    console.log(tbl);   
-    note.map((n, index) => {
-        let row = tbl.insertRow();
-
-        let cell1 = row.insertCell();
-        let text1 = document.createTextNode(n);
-        let cell2 = row.insertCell();
-        let text2 = document.createTextNode(count[index]);
-
-        cell1.appendChild(text1);
-        cell2.appendChild(text2);
-    })
-    
-}
-
-
 
 function noteCounter(denominaton, amount){
     let count = amount / denominaton;
@@ -95,8 +80,7 @@ function calculate(balance){
         message =  "Please pay " + balance + " rs more";
     }else{
         let notes = denominations.filter(n => n <= balance);
-        console.log("notes for this balance: "+ notes);
-        // let noteObj = {};
+        console.log("notes for this balance: " + notes);
         notes.map( n => {
             if(balance >= n){
                 let [noteCount, newBalance] = noteCounter(n, balance);
@@ -110,13 +94,3 @@ function calculate(balance){
 
     }
 }
-
-// generateTable(table, nd, countOfNotes );
-// generateTableHead(table, ["Note", "quantity"] );
-
-// if(message !== ""){
-//     console.log(message);
-// } else{
-//     generateTable(table, nd, countOfNotes );
-//     generateTableHead(table, ["Note", "quantity"] );
-// }
